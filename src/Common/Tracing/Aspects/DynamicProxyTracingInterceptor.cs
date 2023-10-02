71e9be83c84442a4d301469b9e55fe00f432b90a
kgtrue <gammel4@gmail.com>
2023-10-02 19:54:21 +0200

    Added service proxy

---
+++ src/Common/Tracing/Aspects/DynamicProxyTracingInterceptor.cs
@@ -0,0 +1,31 @@
+﻿using OpenTelemetry.Trace;
+using System.Diagnostics;
+using System.Reflection;
+using Castle.DynamicProxy;
+namespace Common.Tracing.Aspects
+{
+    public class DynamicProxyTracingInterceptor : IInterceptor
+    {
+        private readonly Tracer _tracer;
+
+        public DynamicProxyTracingInterceptor(Tracer tracer)
+        {
+            _tracer = tracer;
+        }
+
+        public void Intercept(IInvocation invocation)
+        {            
+            using var span = _tracer.StartActiveSpan($"Calling method {invocation.Method.DeclaringType.Name}.{invocation.Method.Name} with arguments {invocation.Arguments}");
+            try
+            {
+                invocation.Proceed();
+                using var subspan = _tracer.StartSpan($"Finished calling method {invocation.Method.DeclaringType.Name}.{invocation.Method.Name}");
+            }
+            catch (TargetInvocationException exc)
+            {
+                span.RecordException(exc.InnerException);
+                throw exc.InnerException;
+            }
+        }
+    }
+}
